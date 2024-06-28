bc4027bd8679af4cab4e585d5ffbcf70795d3d6b
Vivek9988 <vivek19102004@gmail.com>
2024-06-28 11:44:56 +0530

    first commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+
+  return (
+  <h1>hello my name is vivek </h1>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  
+anotherElement
+  
+)
