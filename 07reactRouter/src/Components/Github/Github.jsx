d3445e2217a5343f01356e5baed2052bcd2bc885
Vivek9988 <vivek19102004@gmail.com>
2024-07-13 21:53:38 +0530

    changes

---
+++ 07reactRouter/src/Components/Github/Github.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Github = () => {
+  return (
+    <div className='text-center m-4 bg-grey-600 text-white p-4 text-3xl'>
+      
+    </div>
+  )
+}
+
+export default Github
